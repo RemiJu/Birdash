c40b4feb073e13e18ee3008ecf7eb0ae42b3ca63
Akaimari <bertrand.marie.pier@gmail.com>
2023-10-10 21:39:48 -0400

    AI meal collision check

--- 01_Unity Project/BirdDash/Assets/AI_Controller.cs
+++ 01_Unity Project/BirdDash/Assets/AI_Controller.cs
@@ -7,17 +7,90 @@ public class AI_Controller : MonoBehaviour
 {
     NavMeshAgent agent;
     public GameObject target;
+    public NewTargetAi targetAi;
+    public float meal;
+    public bool sentMessage;
+
+    public GameObject enemy;
+
+    //set colliderMeal to true if collider has a meal
+    public bool colliderMeal;
+    public bool thisHasMeal;
+
 
     public void Awake()
     {
+        sentMessage = false;
         agent = GetComponent<NavMeshAgent>();
+        targetAi = GetComponentInChildren<NewTargetAi>();
+        colliderMeal = false;
     }
     private void Update()
     {
         GoToTarget();
+
+        if (targetAi.meal > 0) {
+            thisHasMeal = true;
+        }
+        else { thisHasMeal = false; }
+
+        if (thisHasMeal && !colliderMeal && !sentMessage && enemy != null) 
+        {
+            Debug.Log(this.gameObject + " FIGHT with " + enemy + " !");
+            sentMessage = true;
+        }
+        if (!thisHasMeal && colliderMeal && !sentMessage)
+        {
+            Debug.Log(this.gameObject + " FIGHT with " + enemy + " !");
+            sentMessage = true;
+        }
     }
     public void GoToTarget() {
         agent.SetDestination(target.transform.position);
     
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("EnemyBird")) {
+
+            enemy = other.gameObject;
+
+            if (targetAi.meal > 0)
+            {
+                Debug.Log(this.gameObject + " has a meal.");
+                other.SendMessage("IHasMeal");
+            }
+            else
+            {
+                Debug.Log(this.gameObject + " has no meal.");
+                other.SendMessage("IHasNoMeal");
+            }
+        
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("EnemyBird"))
+        {
+            if (colliderMeal)
+            {
+                colliderMeal = false;
+            }
+            sentMessage = false;
+
+            enemy = null;
+
+        }
+    }
+
+    public void IHasMeal() {
+        colliderMeal = true;
+    }
+
+    public void IHasNoMeal() {
+    colliderMeal = false;
+    }
+
 }
